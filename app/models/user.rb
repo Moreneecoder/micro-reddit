e637295f5b09707961c5210f1389057ad52da46f
FdI96 <federicolopezce@gmail.com>
2021-03-23 19:51:53 -0300

    Add the Comment model and its validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :posts
+    has_many :posts, :comments
 
     validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
     validates :email, presence: true, uniqueness: true
