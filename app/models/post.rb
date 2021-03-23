805d04f8327e7eb3dccac3b5044dea59c62099ca
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-23 23:33:28 +0100

    Add association to User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+
     validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
     validates :email, presence: true, uniqueness: true
 end
