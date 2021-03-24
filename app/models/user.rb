9e805ce82117a839ad176abc7f148dd016b85bcb
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-24 17:10:36 +0100

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
+  has_many :posts
+  has_many :comments
 
-    validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
-    validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
