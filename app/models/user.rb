0cb1ea6a3c41751f6bada37124a290f4861675a7
FdI96 <federicolopezce@gmail.com>
2021-03-24 12:25:38 -0300

    Update comment Model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_many :posts, :comments
+    has_many :posts
+    has_many :comments
 
     validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
     validates :email, presence: true, uniqueness: true
